00973a84a18020ad9a76b05493cad4d1a08a5cd7
Michelle Lai <michelle@cir.ca>
2014-07-15 19:48:26 -0700

    Fixed json with trailing last-item commas

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var application_root = __dirname,
     express = require( 'express' ), //Web framework
     path = require( 'path' ), //Utilities for dealing with file paths
     fs = require( 'node-fs' ); //file system
+    mongoose = require('mongoose');
 
 //Create server
 var app = express();
@@ -25,6 +26,7 @@ var app = express();
 //     app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
 // });
 
+
 //Start server
 var port = 4711;
 app.listen( port, function() {
@@ -34,54 +36,83 @@ app.listen( port, function() {
 var post_db = 'mongodb://localhost/post_database';
 var db = mongoose.connect( post_db );
 
+function handleError(err, req, res, next) {
+  // log it
+  console.error(err.stack);
+
+  // respond with 500 "Internal Server Error".
+  res.send(500);
+}
+
 //Schemas
 
+var AuthorSchema =  new mongoose.Schema( {
+    name: String,
+    id: String
+});
+var AccountSchema =  new mongoose.Schema( {
+    id: String,
+    name: String,
+    email: String,
+    member_since: Date,
+    last_seen: Date,
+    num_entries: Number,
+    wins: Number,
+    badges: [String],
+    contests: [String]
+});
 var PostSchema =  new mongoose.Schema( {
     author: AuthorSchema,
     id: String,
     entry: String,
     score: Number
 });
-var AuthorSchema =  new mongoose.Schema( {
-    name: String,
-    id: String
-});
 var ContestSchema = new mongoose.Schema({
     id: String,
     words: [String],
-    time_limit:
-    author: String,
-    releaseDate: Date
+    time_limit: Number,
+    genre: [String],
+    status: String,
+    entries: [PostSchema]
 });
 
 //Models
-var Contest = mongoose.model('Contest', Contest);
-var Post = mongoose.model('Post', Post);
+var Contest = mongoose.model('Contest', ContestSchema);
+var Post = mongoose.model('Post', PostSchema);
+var Author = mongoose.model('Author', AuthorSchema);
+var Account = mongoose.model('Account', AccountSchema);
 
 
-db.on('error', handleError);
+//db.on('error', handleError);
 
-app.get( '/api/posts', function( request, response ) {
-    console.log(request)
-    console.log(response)
-    var author = new Author();
-    author.name = response.name
-    author.save();
-    mockPath = path.join(application_root, '/data-apps.json')
-    result = JSON.parse(fs.readFileSync(mockPath, 'utf8'))
+app.get( '/api/contests', function( request, response ) {
+    console.log(request);
+    console.log(response);
+    var contest = new Contest();
+    contest.id = response.id;
+    contest.words.push(response.words);
+    contest.time_limit = response.time_limit;
+    contest.genre.push(response.genre);
+    contest.status = response.status;
+    contest.entries = response.entries;
+    contest.save();
+    mockPath = path.join(application_root, '/data-contests.json');
+    result = JSON.parse(fs.readFileSync(mockPath, 'utf8'));
     response.json(result);
 });
 
-app.get( '/api/students', function( request, response ) {
-
+app.get( '/api/accounts', function( request, response ) {
+    var account = new Account();
+    account.id = response.id;
+    account.name = response.name;
+    account.email = response.email;
+    account.member_since = response.email_since;
+    account.last_seen = response.last_seen;
+    account.num_entries = response.num_entries;
+    account.wins = response.wins;
+    account.badges.push(response.badges);
+    account.contests.push(response.contests);
     mockPath = path.join(application_root, '/data-students.json')
-    result = JSON.parse(fs.readFileSync(mockPath, 'utf8'))
-    response.json(result);
-});
-
-app.get( '/api/sections', function( request, response ) {
-
-    mockPath = path.join(application_root, '/data-sections.json')
     result = JSON.parse(fs.readFileSync(mockPath, 'utf8'))
     response.json(result);
 });
